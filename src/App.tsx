3ec617cb8faa624f3d3f9592ec53f1632daaae78
LeeTaeGwan <taegwan.lee@ssvt.co.kr>
2022-11-22 13:39:10 +0900

    add json-server lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import { useQueryClient } from '@tanstack/react-query';
 import Todos from './Todos';
 
 function App() {
+  const queryClient2 = useQueryClient();
+  console.log(queryClient2);
+
   return (
     <div className="App">
       <Todos />
--- src/Todos.tsx
+++ src/Todos.tsx
@@ -1,22 +1,35 @@
-import { useQuery } from '@tanstack/react-query';
-import { useEffect, useState } from 'react';
+import { useQuery, UseQueryResult } from '@tanstack/react-query';
+
+interface DataType {
+  id: number
+  name?: string
+  age?: string
+}
 
 const fetchTodoList = async () => {
-  const data = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+  const data = await fetch('http://localhost:4000/user')
     .then((res) => res.json());
   return data;
 };
 
 function Todos() {
-  const [data, setData] = useState(null);
+  const { data } = useQuery<DataType[]>(['todos'], fetchTodoList);
+  console.log(useQuery<DataType[]>(['todos'], fetchTodoList));
 
-  useEffect(() => {
-    fetchTodoList().then((res) => setData(res));
-  }, []);
+  // const result = useQuery<DataType[]>(['todos'], fetchTodoList);
 
+  // if (!data) return null;
   return (
-    <div />
+    <div>
+      {data?.map((el) => <div key={el.name}>{el.name}</div>)}
+      {/* {data && <div>{data.age}</div>} */}
+    </div>
   );
 }
 
 export default Todos;
+
+// 데이터 패칭시 처음에 undefined인거 해결법
+// 데이터가 배열이면 옵셔널체이닝
+// 조건문 early return
+// &&연산자 사용
